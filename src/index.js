379cb3ebf647e4158a8e0d31295ed484b0f1a061
mwas <emmamwas99@gmail.com>
2021-01-15 09:29:31 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import { GraphQLServer } from 'graphql-yoga';
+
+//set up the type definations(Schema)
+// location
+// bio
+
+const typeDefs = `
+type Query {
+    hello:String!
+    man:String!
+    bio:String!
+    location:String!
+}
+`;
+
+const resolvers = {
+  Query: {
+    hello() {
+      return 'hello world';
+    },
+    man() {
+      return 'This is a man mama';
+    },
+    bio() {
+      return 'I am Emmanuel mwangi maina.A senior frontend developer with over 3 years working exprience';
+    },
+    location() {
+      return 'I come from Nairobi Kenya but currently living in Boston USA';
+    },
+  },
+};
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log('running on port 4000');
+});
+
+//setup the resolvers(functions to be called)
